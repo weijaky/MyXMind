a3fbd570d1255b0cc3d85de69423faf0e6a94c4a
jaky <weijunjie@onyx-international.com>
2018-02-09 19:48:06 +0800

    update.

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 git status
 git add -A .
 git commit -m "update."
-exec ssh-agent bash
-eval ssh-agent -s
-exec ssh-add ~/.ssh/id_rsa_weijaky
+ssh-agent
+ssh-agent -s
+ssh-add ~/.ssh/id_rsa_weijaky
 git push jaky master
